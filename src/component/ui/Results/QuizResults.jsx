b67d715c47ffa5c0ecb25a1ab959ffd23c8a619b
Mayor Creator <paulaboagye@outlook.com>
2025-01-23 21:57:13 -0500

    Refactor QuizResults component; enhance layout and styling for improved user experience

--- src/component/ui/Results/QuizResults.jsx
+++ src/component/ui/Results/QuizResults.jsx
@@ -1,4 +1,5 @@
 import { QuizButton } from "../QuizMenuButton/QuizButton";
+import styles from "./QuizResults.module.css";
 
 export function QuizResult({
   score,
@@ -9,26 +10,35 @@ export function QuizResult({
 }) {
   return (
     <>
-      <div>
-        <h1>
-          Quiz completed <span>You scored...</span>
-        </h1>
-      </div>
-      <div>
-        <div>
-          <div>
-            <img src={icon} alt={`icon-${title}`} />
-          </div>
-          <div>
-            <h2>{title}</h2>
-          </div>
+      <main>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.headerTitle}>
+            Quiz completed{" "}
+            <span className={styles.scoreTitle}>You scored...</span>
+          </h1>
         </div>
-        <div>
-          <h2>{score}</h2>
-          <p>out of {questions.length}</p>
+        <div className={styles.scoreCardContainer}>
+          <div className={styles.scoreCardChildContainer}>
+            <div className={styles.titleContainer}>
+              <div>
+                <img src={icon} alt={`icon-${title}`} />
+              </div>
+              <div>
+                <h2 className={styles.title}>{title}</h2>
+              </div>
+            </div>
+            <div>
+              <h2 className={styles.scoreText}>{score}</h2>
+            </div>
+            <div>
+              <p className={styles.scoreTotal}>out of {questions.length}</p>
+            </div>
+          </div>
+          <QuizButton className={styles.submitBtn} onClick={handleRestartQuiz}>
+            Play Again
+          </QuizButton>
         </div>
-        <QuizButton onClick={handleRestartQuiz}>Play Again</QuizButton>
-      </div>
+      </main>
     </>
   );
 }
