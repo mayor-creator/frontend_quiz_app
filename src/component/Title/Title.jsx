48007b46a9cd9c4f660187dccee7a8ef60e7e379
Mayor Creator <paulaboagye@outlook.com>
2025-03-04 12:59:59 -0500

    Enhance Title component: Add PropTypes for type checking, refactor JSX structure for clarity, and improve styles for responsiveness and accessibility.

--- src/component/Title/Title.jsx
+++ src/component/Title/Title.jsx
@@ -1,22 +1,32 @@
+import PropTypes from "prop-types";
 import { ThemeToggle } from "../Theme/Theme";
 import styles from "./Title.module.css";
 
 export function Title({ title, icon }) {
   return (
-    <>
+    <header className={styles.header}>
       <div className={styles.titleContainer}>
-        <div>
-          <img src={icon} alt={`icon-${title}`} />
+        <div className={styles.iconWrapper}>
+          <img
+            src={icon}
+            alt={`${title} quiz icon`}
+            className={styles.icon}
+            width="40"
+            height="40"
+          />
         </div>
-        <div>
+        <div className={styles.titleWrapper}>
           <h1 className={styles.title}>{title}</h1>
         </div>
       </div>
-      <div>
+      <div className={styles.themeToggleWrapper}>
         <ThemeToggle />
       </div>
-    </>
+    </header>
   );
 }
 
-Title.propTypes;
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
