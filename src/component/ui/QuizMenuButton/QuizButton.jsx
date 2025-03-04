9b85f4459d5e552b36aead04c6d98cfc24d1de37
Mayor Creator <paulaboagye@outlook.com>
2025-03-04 12:31:36 -0500

    Enhance QuizButton component: Refactor to use memo for performance optimization, add disabled state handling, and implement PropTypes for type checking

--- src/component/ui/QuizMenuButton/QuizButton.jsx
+++ src/component/ui/QuizMenuButton/QuizButton.jsx
@@ -1,11 +1,28 @@
 import styles from "./QuizButton.module.css";
+import PropTypes from "prop-types";
+import { memo } from "react";
 
-export function QuizButton({ className = "", children, ...delegated }) {
+export const QuizButton = memo(function QuizButton({
+  className = "",
+  children,
+  disabled,
+  ...delegated
+}) {
   return (
-    <button className={`${styles.button} ${className}`} {...delegated}>
+    <button
+      className={`${styles.button} ${className}`}
+      type="button"
+      disabled={disabled}
+      aria-disabled={disabled}
+      {...delegated}
+    >
       {children}
     </button>
   );
-}
+});
 
-QuizButton.propTypes;
+QuizButton.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+};
