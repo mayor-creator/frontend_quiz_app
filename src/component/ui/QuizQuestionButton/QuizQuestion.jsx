0aa22e0b07b1fdcb3ed35c2496faf25c9ca9c1ca
Mayor Creator <paulaboagye@outlook.com>
2025-03-04 12:19:39 -0500

    Enhance QuizQuestion component: Add PropTypes for type checking and default values for className and isSelected

--- src/component/ui/QuizQuestionButton/QuizQuestion.jsx
+++ src/component/ui/QuizQuestionButton/QuizQuestion.jsx
@@ -1,31 +1,35 @@
 import styles from "./QuizQuestion.module.css";
+import PropTypes from "prop-types";
 
 export function QuizQuestion({
-  className,
+  className = "",
   type,
   children,
-  isSelected,
+  isSelected = false,
   ...delegated
 }) {
   return (
-    <>
-      <button
-        className={`${styles.button} ${className} ${
-          isSelected ? styles.selected : ""
+    <button
+      className={`${styles.button} ${className} ${
+        isSelected ? styles.selected : ""
+      }`}
+      {...delegated}
+    >
+      <div
+        className={`${styles.questionTypeBox} ${
+          isSelected ? styles.selectedBox : ""
         }`}
-        {...delegated}
       >
-        <div
-          className={`${styles.questionTypeBox} ${
-            isSelected ? styles.selectedBox : ""
-          }`}
-        >
-          {type}
-        </div>
-        <div className={styles.questionBox}>{children}</div>
-      </button>
-    </>
+        {type}
+      </div>
+      <div className={styles.questionBox}>{children}</div>
+    </button>
   );
 }
 
-QuizQuestion.propTypes;
+QuizQuestion.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  isSelected: PropTypes.bool,
+};
